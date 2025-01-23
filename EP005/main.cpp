5df72ecb98570e43ce02547a46fdeacf7bef43b6
jugufugu <utkarsh.asthana55@gmail.com>
2025-01-24 00:08:52 +0530

    Use of multiple cpp files using cmake

---
+++ EP005/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "physics.h"
+#include  "burrito.h"
+using namespace std;
+int main()
+{
+burrito b;
+cout<<"Learn using CMake";
+}
---
+++ EP005/src/burrito.cpp
@@ -0,0 +1,9 @@
+#include "burrito.h"
+#include <iostream>
+// this file needs iostream and namsespace std
+using namespace std;
+
+burrito::burrito()
+{
+   cout << "I am apple" << endl;
+}
